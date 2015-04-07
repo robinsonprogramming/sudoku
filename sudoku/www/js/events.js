ebce7ec310a3994316b74dc714335a99db951c7c
password <password@debian>
2015-04-07 00:36:49 -0500

    remove sudoku subdirectory

--- sudoku/www/js/events.js
+++
@@ -1,9 +0,0 @@
-function color(color){
-     $("#color-menu").css("color", color);
-     $("#color").val(color);
-}
-function difficulty(difficulty,text){
-    $("#difficulty").val(difficulty);
-    $("#difficulty-display").html(text);
-    startNewGame();
-}
